ef3c28d3554aeaeb0dec27318a9be11d732811a9
Ryan <49207055+RyanS42@users.noreply.github.com>
2020-08-14 13:51:05 -0700

    Update Reverse_arrary.java

--- Reverse_arrary.java
+++ Reverse_arrary.java
@@ -8,14 +8,14 @@ public class Reverse_arrary {
      
     //print array starting from first element
     System.out.println("Original Array:");
-    for(int i=0; i<intArray.length; i++)
+    for(int i = 0; i < intArray.length; i++)
          System.out.print(intArray[i] + "  ");
      
     System.out.println();
      
     //print array starting from last element
     System.out.println("Original Array printed in reverse order:");
-         for(int i=intArray.length-1; i>=0; i--)
+         for(int i = intArray.length-1; i >= 0; i--)
          System.out.print(intArray[i] + "  ");
 	    }
 	}
